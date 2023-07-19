c58092cda597ba6842b5f774e4e6162d652df0c7
Yiting Wang <yitingw@mathworks.com>
2023-07-19 16:55:01 -0400

    try non-premade

--- buildfile.m
+++ buildfile.m
@@ -8,19 +8,19 @@ function plan = buildfile
 plan = buildplan(localfunctions);
 plan("dummy1") = Task(Actions=@(~) disp('This is a dummy task'));
 
-// % lint, mex, pcode, test
+% lint, mex, pcode, test
 
-// plan("lint") = CodeIssuesTask(["src","tests"], WarningThreshold=4, Results="issues.mat");
-// plan("mex") = MexTask("mex/*.c", "mex");
+% plan("lint") = CodeIssuesTask(["src","tests"], WarningThreshold=4, Results="issues.mat");
+% plan("mex") = MexTask("mex/*.c", "mex");
 
-// plan("pcode") = PcodeTask("src/*.m", pwd);
-// %pcode task depends on lint and mex tasks
-// plan("pcode").Dependencies = ["lint", "mex"];
+% plan("pcode") = PcodeTask("src/*.m", pwd);
+% pcode task depends on lint and mex tasks
+% plan("pcode").Dependencies = ["lint", "mex"];
 
-// plan("test") = TestTask("tests", TestResults="test-results/report.pdf", SourceFiles="src");
+% plan("test") = TestTask("tests", TestResults="test-results/report.pdf", SourceFiles="src");
 
-//plan("clean") = CleanTask();
-//plan.DefaultTasks = ["lint" "mex" "pcode" "test"];
+% plan("clean") = CleanTask();
+% plan.DefaultTasks = ["lint" "mex" "pcode" "test"];
 plan.DefaultTasks = ["dummy1"];
 
 end
